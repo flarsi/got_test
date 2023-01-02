6f9bcc29eb894e2b05b81842ea11b8dbdda4a151
= <danil.zap63.dd@gmail.com>
2023-01-02 10:56:33 +0200

    styling-fixes

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -3,9 +3,13 @@ import MoreInfo from "./components/MoreInfo";
 import "./index.css";
 
 function Card({ title, image, moreInfo }) {
+  const imgStyle = {
+    backgroundImage: "url(" + image + ")",
+  };
+
   return (
     <div className="card">
-      <img className="card-image" src={image} alt="" />
+      <div className="card-image" style={imgStyle} alt=""></div>
       <div className="card-content">
         <div className="card-title">{title}</div>
         <div className="card-more-info">
